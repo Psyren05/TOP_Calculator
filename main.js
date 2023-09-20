53a9c2a7c74bb5681e8f30e4be550a14a2951703
Lishan <lishan.low11@gmail.com>
2023-09-20 12:45:56 +0100

    added html buttons, style and basic functions

---
+++ main.js
@@ -0,0 +1,24 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator;
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate(firstNumber, secondNumber) {
+    // if add button is pressed, call the add() function return add(firstNumber, secondNumber)
+}
+
