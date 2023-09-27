9df61ae6f2b55e9bcde21db5edef8a9cca7ab38e
Lishan <lishan.low11@gmail.com>
2023-09-27 19:23:16 +0100

    wrote some of the operaate() function

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-let firstNumber = 0;
-let secondNumber = 0;
-let operator;
+let firstNumber = 4;
+let secondNumber = 8;
+let currentOperator;
+let result;
 
-function add(a, b) {
-    return a + b
+const displayWindow = document.getElementById('display-window');
+
+const addBtn = document.getElementById('add-btn');
+const addBtnClicked = addBtn.addEventListener('click', () => {
+    currentOperator = '+';
+});
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
 }
 
-function subtract(a, b) {
-    return a - b
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber
 }
 
-function multiply(a, b) {
-    return a * b
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber
 }
 
-function divide(a, b) {
-    return a / b
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber
 }
 
 function operate(firstNumber, secondNumber) {
     // if add button is pressed, call the add() function and return add(firstNumber, secondNumber)
+    if (currentOperator === '+') {
+        result = add(firstNumber, secondNumber);
+        // update the display window
+        displayWindow.innerText === result;
+        return result
+    }
 }
-
