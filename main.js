2bc9713fbb23c3a3f435415aea8bf18bd4e032a1
Lishan <lishan.low11@gmail.com>
2023-09-28 15:34:42 +0100

    added event listeners to operator buttons and completed the operate function

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
-let firstNumber = 4;
-let secondNumber = 8;
+let firstNumber;
+let secondNumber;
 let currentOperator;
 let result;
 
 const displayWindow = document.getElementById('display-window');
 
 const addBtn = document.getElementById('add-btn');
+const subtractBtn = document.getElementById('subtract-btn');
+const multiplyBtn = document.getElementById('multiply-btn');
+const divideBtn = document.getElementById('divide-btn');
+
 const addBtnClicked = addBtn.addEventListener('click', () => {
     currentOperator = '+';
+});
+const subtractBtnClicked = subtractBtn.addEventListener('click', () => {
+    currentOperator = '-';
+});
+const multiplyBtnClicked = multiplyBtn.addEventListener('click', () => {
+    currentOperator = 'X';
+});
+const divideBtnClicked = divideBtn.addEventListener('click', () => {
+    currentOperator = '/';
 });
 
 function add(firstNumber, secondNumber) {
@@ -26,12 +39,24 @@ function divide(firstNumber, secondNumber) {
     return firstNumber / secondNumber
 }
 
-function operate(firstNumber, secondNumber) {
+function operate(firstNumber, secondNumber, currentOperator) {
     // if add button is pressed, call the add() function and return add(firstNumber, secondNumber)
     if (currentOperator === '+') {
         result = add(firstNumber, secondNumber);
-        // update the display window
-        displayWindow.innerText === result;
-        return result
-    }
+    } else if (currentOperator === '-') {
+        result = subtract(firstNumber, secondNumber);
+    } else if (currentOperator === 'X') {
+        result = multiply(firstNumber, secondNumber);
+    } else if (currentOperator === '/') {
+        result = divide(firstNumber, secondNumber);
+
+        displayWindow.innerText = result;
+    }   return result
+}
+// Create the functions that populate the display when you click 
+// the number buttons. You should be storing the ‘display value’ in 
+// a variable somewhere for use in the next step.
+
+function populateDisplay {
+    
 }
